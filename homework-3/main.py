87b723ddffa8a1984f4bca2eec1ea56a4ca6a533
kirillskyeng <117906913+kirillskyeng@users.noreply.github.com>
2023-03-21 13:52:22 +0300

    Update main.py

--- homework-3/main.py
+++ homework-3/main.py
@@ -10,4 +10,9 @@ if __name__ == '__main__':
     print(vdud + redactsiya)  # 13970000
     print(vdud - redactsiya)  # 6630000
     print(redactsiya - vdud)  # -6630000
+    print(vdud > redactsiya)  # True
     print(vdud >= redactsiya)  # True
+    print(vdud < redactsiya)  # False
+    print(vdud <= redactsiya)  # False
+    print(vdud == redactsiya)  # False
+    
