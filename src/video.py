90544329ed4d10cd913e0ba925043d82685a0e88
nikmaxon <nikmakson99@gmail.com>
2023-05-03 23:12:23 +0300

    Homework 6 done.

--- src/video.py
+++ src/video.py
@@ -4,18 +4,32 @@ import os
 from googleapiclient.discovery import build
 
 
+# from googleapiclient.errors import HttpError
+
+class HttpError(Exception):
+    pass
+
+
 class Video:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YouTube API')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id: str):
-        self.video_id = video_id
-        self.video_info = self.get_info()
-        self.video_name = self.video_info["items"][0]["snippet"]["title"]
-        self.video_link = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.video_views = self.video_info["items"][0]["statistics"]["viewCount"]
-        self.video_likes = self.video_info["items"][0]["statistics"]["likeCount"]
+        try:
+            self.video_id = video_id
+            self.video_info = self.get_info()
+            self.video_name = self.video_info["items"][0]["snippet"]["title"]
+            self.video_link = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.video_views = self.video_info["items"][0]["statistics"]["viewCount"]
+            self.video_likes = self.video_info["items"][0]["statistics"]["likeCount"]
+        except HttpError:
+            self.video_info = None
+            self.video_name = None
+            self.video_link = None
+            self.video_views = None
+            self.video_likes = None
+            print("Неверное ID видеоролика")
 
     def __str__(self):
         return self.video_name
@@ -23,9 +37,12 @@ class Video:
     def get_info(self):
         """Выводит в консоль информацию о канале."""
         video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                       id=self.video_id
-                                       ).execute()
-        return video_response
+                                                    id=self.video_id
+                                                    ).execute()
+        if len(video_response['items']) == 0:
+            raise HttpError
+        else:
+            return video_response
 
 
 class PLVideo(Video):
